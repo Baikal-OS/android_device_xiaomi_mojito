d951d0739d353069665c5b69902956713822650a
Sachinpawar86 <sachinbarange86@gmail.com>
2025-10-03 21:42:56 +0530

    Revert "Hello Superior"
    This reverts commit 428a0c6787902ef6dec2e7b7694038969d1fdc95.

--- lineage_mojito.mk
+++ lineage_mojito.mk
@@ -11,8 +11,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from mojito device
 $(call inherit-product, device/xiaomi/mojito/device.mk)
 
-# Inherit some common SuperiorOS stuff.
-$(call inherit-product, vendor/superior/config/common.mk)
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Boot Animation Resolution
 TARGET_BOOT_ANIMATION_RES := 1080
@@ -22,7 +22,7 @@ BUILD_WITH_GAPPS := false
 SUPERIOR_EDITION := Vanilla
 TARGET_USES_AOSP_RECOVERY := true
 
-PRODUCT_NAME := superior_mojito
+PRODUCT_NAME := lineage_mojito
 PRODUCT_DEVICE := mojito
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 10
